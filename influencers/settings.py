52d1a6313a934f7f6facde197926c567bc9dd0ac
Nemtsov <mrtwister5950@gmail.com>
2020-05-21 23:08:01 +0300

    changed some stylesto buttons

--- influencers/settings.py
+++ influencers/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'influencers.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'influencers',
-        'USER': 'influencer',
-        'PASSWORD': 'influencer',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
