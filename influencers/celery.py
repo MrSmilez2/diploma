f27fb5839a59aeb2bb9dd2bebafc243c5e0160e3
Nemtsov <mrtwister5950@gmail.com>
2020-07-14 02:30:12 +0300

    fixed celery

--- influencers/celery.py
+++ influencers/celery.py
@@ -1,10 +1,13 @@
 import os
 import django
+
 from celery import Celery
-from influencers_app.tasks import SCHEDULE as INFLUENCERS_APP_SCHEDULE
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'influencers.settings')
 django.setup()
+
+from influencers_app.tasks import SCHEDULE as INFLUENCERS_APP_SCHEDULE
+
 app = Celery('influencers')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
