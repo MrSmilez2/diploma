252f8764175e9cb1552c273d1aeaa718ef316b4e
Nemtsov <mrtwister5950@gmail.com>
2020-09-22 00:00:45 +0300

    refactored enums

--- influencers_app/enums.py
+++ influencers_app/enums.py
@@ -2,17 +2,17 @@ from django.utils.translation import gettext_lazy as _
 from enum import IntEnum
 
 
-class Progress(IntEnum):
+class ProgressType(IntEnum):
 
-    REVIEW_DONE = 1, _('Review done')
-    AWAITING_REVIEW = 2, _('Awaiting review')
-    PRODUCTS_SENT = 3, _('Products sent')
-    COMMUNICATING = 4, _('Communicating')
-    OFFER_DECLINED = 5, _('Offer declined')
-    REJECTION = 6, _('Rejection')
-    EMAIL_INQUIRY_SENT = 7, _('Email inquiry sent')
-    ON_HOLD = 8, _('On hold')
-    DEFAULT_VALUE = 9, _('Default')
+    REVIEW_DONE = 1
+    AWAITING_REVIEW = 2
+    PRODUCTS_SENT = 3
+    COMMUNICATING = 4
+    OFFER_DECLINED = 5
+    REJECTION = 6
+    EMAIL_INQUIRY_SENT = 7
+    ON_HOLD = 8
+    DEFAULT_VALUE = 9
 
     @classmethod
     def choices(cls):
